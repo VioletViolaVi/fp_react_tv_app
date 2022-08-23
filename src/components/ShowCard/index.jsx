26c0b575f9a527485aa55d84631da1288fc17ecc
vivian <vivian.v18723@gmail.com>
2022-08-23 15:50:02 +0100

    edit notes in comments

--- src/components/ShowCard/index.jsx
+++ src/components/ShowCard/index.jsx
@@ -1,20 +1,39 @@
+/*
+NOTES:      
+  - only run this instruction (on the right) if the code on the left is true
+  - same thing as the ternary operator code below 
+  - da code:
+    - '()' were used as code jumped to new line otherwise, no '()'
+  {data.image && (
+    <img src={data["image"]["medium"]} alt="Image of tv show." />
+  )}
+  - another way (for above):
+    - <p>dangerouslySetInnerHTML = {{ __html: data.summary }}</p>       
+    
+    
+    
+  - another way (to whole code): 
+  import React from 'react';
+
+  const ShowCard = ({ data }) => {
+      return (
+        <div className="show-card">
+          <img src={ data["image"] ? data["image"]["medium"] : ""}></img>
+          <h2>{data.name}</h2>
+          <p dangerouslySetInnerHTML={{__html: data.summary}}></p>
+        </div>
+      )
+  }
+
+  export default ShowCard;
+*/
+
 import React from "react";
 
 const ShowCard = ({ data }) => {
   console.log(data);
   return (
     <div className="show_card">
-      {/*
-        - only run this instruction (on the right) if the code on the left is true
-        - same thing as the ternary operator code below 
-        - da code:
-          - '()' were used as code jumped to new line otherwise, no '()'
-        {data.image && (
-          <img src={data["image"]["medium"]} alt="Image of tv show." />
-        )}
-        - another way:
-          - <p>dangerouslySetInnerHTML = {{ __html: data.summary }}</p> 
-      */}
       <img
         src={data["image"] ? data["image"]["medium"] : ""}
         alt="Image of tv show."
@@ -26,20 +45,3 @@ const ShowCard = ({ data }) => {
 };
 
 export default ShowCard;
-
-/*
-- another way: 
-import React from 'react';
-
-const ShowCard = ({ data }) => {
-    return (
-      <div className="show-card">
-        <img src={ data["image"] ? data["image"]["medium"] : ""}></img>
-        <h2>{data.name}</h2>
-        <p dangerouslySetInnerHTML={{__html: data.summary}}></p>
-      </div>
-    )
-}
-
-export default ShowCard;
-*/
