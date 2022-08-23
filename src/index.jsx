60a392c17d1d49c46296a2f803f79383ec7cc96e
vivian <vivian.v18723@gmail.com>
2022-08-23 09:53:54 +0100

    create Index.jsx file with start up code

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+// things from code that's not mine
+import { createRoot } from "react-dom/client";
+import React from "react";
+// space
+
+// stuff from my code
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
