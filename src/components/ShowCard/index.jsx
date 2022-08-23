34445e7001ee66f3e2856c597b9d5f504dc9ca42
vivian <vivian.v18723@gmail.com>
2022-08-23 12:13:32 +0100

    add notes use ternary operator to display image if present... ...otherwise an empty string add summary from api

--- src/components/ShowCard/index.jsx
+++ src/components/ShowCard/index.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 const ShowCard = ({ data }) => {
+  console.log(data);
   return (
     <div className="show_card">
+      {/*
+        - only run this instruction (on the right) if the code on the left is true
+        - same thing as the ternary operator code below 
+        - da code:
+          - '()' were used as code jumped to new line otherwise, no '()'
+        {data.image && (
+          <img src={data["image"]["medium"]} alt="Image of tv show." />
+        )}
+      */}
+      <img
+        src={data["image"] ? data["image"]["medium"] : ""}
+        alt="Image of tv show."
+      />
       <h2>{data.name}</h2>
+      <p>{data.summary}</p>
     </div>
   );
 };
