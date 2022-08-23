f91027460f259605676f26ee91a3d0e14c8f2e01
vivian <vivian.v18723@gmail.com>
2022-08-23 12:12:26 +0100

    create new component passing prop... ...sent from <SearchForm/> in App.jsx use useState to create state variable... ...based on user input add form with function names

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SearchForm = () => {
+const SearchForm = ({ handleSearchSubmission }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleInput(event) {
+    const newInput = event.target.value;
+    setInputValue(newInput);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleSearchSubmission(inputValue);
+  }
+
   return (
     <>
-      <form action="#" id="searchForm">
-        <input type="text" name="" id="" />
+      <form action="#" id="searchForm" onSubmit={handleSubmit}>
+        <label htmlFor="searchBox">Query: </label>
+        <input
+          type="text"
+          onChange={handleInput}
+          value={inputValue}
+          id="searchBox"
+          required
+          name=""
+        />
         <input type="submit" value="Send Off" />
       </form>
     </>
