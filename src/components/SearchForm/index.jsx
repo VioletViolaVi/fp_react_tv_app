470d084ed59d63aa14cb22443a23d886dbb49cd7
vivian <vivian.v18723@gmail.com>
2022-08-23 11:16:45 +0100

    change name of files to index.jsx. move files to own folders for better structuring

---
+++ src/components/SearchForm/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SearchForm = () => {
+  return (
+    <>
+      <form action="#" id="searchForm">
+        <input type="text" name="" id="" />
+        <input type="submit" value="Send Off" />
+      </form>
+    </>
+  );
+};
+
+export default SearchForm;
