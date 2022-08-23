c73237eeac4c38655762769713ffa9e39ac27728
vivian <vivian.v18723@gmail.com>
2022-08-23 16:37:31 +0100

    edit file

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <h1>Matt's TV Extravaganza</h1>
+      <h1>Merideth's TV Extravaganza</h1>
       <em>Copyright gleefully violated</em>
     </header>
   );
