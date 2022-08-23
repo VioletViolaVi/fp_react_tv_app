0bf04ab407d9b1a7415224464817174037286549
vivian <vivian.v18723@gmail.com>
2022-08-23 10:59:28 +0100

    pass through data as props

--- src/components/ShowCard.jsx
+++ src/components/ShowCard.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const ShowCard = ({ title, description, rating }) => {
+const ShowCard = ({ data }) => {
   return (
     <div className="show_card">
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <div>{rating}/10</div>
+      <h2>{data.name}</h2>
     </div>
   );
 };
