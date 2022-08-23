0e316e3bb7fc7fc38bdf10208c23cfc3a3bc29e0
vivian <vivian.v18723@gmail.com>
2022-08-23 16:36:03 +0100

    write 1st passing test for Header component

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
+import Header from "./index";
+
+describe("Header component", () => {
+  it("Displays a h1 w/ appropriate text", () => {
+    render(<Header />);
+
+    const heading = screen.queryByRole("heading");
+
+    expect(heading).toBeInTheDocument();
+  });
+});
