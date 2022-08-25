6639bf97165bff9724e7f92a1a7987855668cd4a
vivian <vivian.v18723@gmail.com>
2022-08-25 17:41:31 +0100

    import & use composeWithDevTools

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
-import { createStore } from "redux";
-// want to interact w/ dev tools inside browser ==> 'devToolsEnhancer
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+// want to interact w/ dev tools inside browser ==> 'devToolsEnhancer'
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "./reducers/reducer";
 
-const store = createStore(reducer, devToolsEnhancer());
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
