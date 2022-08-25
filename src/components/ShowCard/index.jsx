b8a9bf761c606506900e84c5d7cfee45d34495a9
vivian <vivian.v18723@gmail.com>
2022-08-25 16:32:43 +0100

    create & export ShowCard

--- src/components/ShowCard/index.jsx
+++ src/components/ShowCard/index.jsx
@@ -1,21 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import { FavouriteButton } from '../'
+import { addShowAction, removeShowAction } from "../../actions";
+
+import { FavouriteButton } from "../";
 
 const ShowCard = ({ data }) => {
+  const saved = useSelector((state) =>
+    state.watchlist.some((show) => show.id == data.id)
+  );
+  const dispatch = useDispatch();
 
-    const [saved, setSaved] = useState(false);
-
-    function toggleSaved () {
-        setSaved(prev => !prev);
+  function toggleSaved() {
+    if (saved) {
+      dispatch(removeShowAction(data.id));
+    } else {
+      dispatch(addShowAction(data));
     }
+  }
 
-    return <div className="show-card" role="figure">
-                {data["image"] && <img src={data["image"]["medium"]}></img>}
-                <h2>{data.name}</h2>
-                <p role="paragraph">{data.summary}</p>
-                <FavouriteButton status={saved ? "saved" : ""} action={toggleSaved} />
-            </div>
-}
+  return (
+    <div className="show-card" role="figure">
+      {data["image"] && <img src={data["image"]["medium"]}></img>}
+      <h2>{data.name}</h2>
+      <p role="paragraph">{data.summary}</p>
+      <FavouriteButton status={saved ? "saved" : ""} action={toggleSaved} />
+    </div>
+  );
+};
 
 export default ShowCard;
+
+/*import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { addShowAction, removeShowAction } from "../../actions";
+
+import { FavouriteButton } from "../";
+
+const ShowCard = ({ data }) => {
+  const saved = useSelector((state) => {
+    return state.watchlist.some((show) => {
+      return show.id == data.id;
+    });
+  });
+  const dispatch = useDispatch();
+
+  function toggleSaved() {
+    setSaved((prev) => !prev);
+    if (saved) {
+      dispatch(removeShowAction(data));
+    } else {
+      dispatch(addShowAction(data));
+    }
+  }
+
+  return (
+    <div className="show-card" role="figure">
+      {data["image"] && <img src={data["image"]["medium"]}></img>}
+      <h2>{data.name}</h2>
+      <p role="paragraph">{data.summary}</p>
+      <FavouriteButton status={saved ? "saved" : ""} action={toggleSaved} />
+    </div>
+  );
+};
+
+export default ShowCard;
+*/
